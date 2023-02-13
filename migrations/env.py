46187875b15b08cd71ade0fff3a5de12c28526b1
Vadym <glushko311@gmail.com>
2023-02-13 13:59:34 +0200

    add auth due fastapi_users

--- migrations/env.py
+++ migrations/env.py
@@ -5,8 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from src.models import metadata
+from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS, JWT_SECRET
+from src.models.models import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,6 +18,9 @@ config.set_section_option(section, "DB_PORT", DB_PORT)
 config.set_section_option(section, "DB_USER", DB_USER)
 config.set_section_option(section, "DB_NAME", DB_NAME)
 config.set_section_option(section, "DB_PASS", DB_PASS)
+config.set_section_option(section, "JWT_SECRET", JWT_SECRET)
+config.set_section_option(section, "RESET_VERIFY_PASS_SECRET", RESET_VERIFY_PASS_SECRET)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- src/app.py
+++ src/app.py
@@ -1,47 +1,40 @@
-import secrets
-from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi_users import FastAPIUsers
+from fastapi import FastAPI, Depends
 
-from src.schemas import User as UserSchema
-from src.models import User
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
 
-from src.db import get_db
+app = FastAPI(
+    title="Trading App"
+)
 
-security = HTTPBasic()
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
-app = FastAPI()
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Phonebook"}
-
-
-@app.get("/smoke")
-async def smoke():
-    return {"message": "smoke complete"}
-
-
-async def authenticate(credential: HTTPBasicCredentials = Depends(security)):
-    is_user_ok = secrets.compare_digest(credential.username, 'unicorn')
-    is_password_ok = secrets.compare_digest(credential.password, 'rainbow')
-    if not (is_user_ok and is_password_ok):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect credentials",
-            headers={"WWW-Authenticate": "Basic"}
-        )
+current_user = fastapi_users.current_user()
 
 
-@app.post("/user", response_model=UserSchema, dependencies=[Depends(authenticate)])
-async def add_user(user: UserSchema, db: Session = Depends(get_db)):
-    return User.create_user(db=db, user=user)
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
 
 
-@app.get("/user", response_model=UserSchema, dependencies=[Depends(authenticate)])
-async def get_user(last_name: str, db: Session = Depends(get_db)):
-    response = User.get_user_by_lastname(db=db, lastname=last_name)
-    if response:
-        return response
-    raise HTTPException(status_code=404, detail=f"User with lastname {last_name} not found")
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, anonym"
